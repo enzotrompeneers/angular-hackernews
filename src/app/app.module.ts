9f296c9ddf315be9c6b30e1c2c43bb36bddec936
enzotrompeneers <trompeneers@telenet.be>
2017-01-15 10:20:56 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StoriesComponent } from './stories/stories.component';
+import { FooterComponent } from './footer/footer.component';
+import { ItemComponent } from './item/item.component';
+
+import { HackerNewsAPIService } from './hackernews-api.service';
+import { ItemCommentsComponent } from './item-comments/item-comments.component';
+//import {MomentModule} from 'angular2-moment';
+//import { DomainPipe } from './domain.pipe';
+import { routing } from './app.routes';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    StoriesComponent,
+    FooterComponent,
+    ItemComponent,
+    ItemCommentsComponent,
+    //DomainPipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    //MomentModule
+    routing,
+  ],
+  providers: [HackerNewsAPIService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
